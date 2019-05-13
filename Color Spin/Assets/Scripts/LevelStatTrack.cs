db051e2eff5c4bf76a4672ddc476e63f266e9aa2
Tony Mays <tonymthediggity@gmail.com>
2019-05-12 23:41:16 -0500

    Basic leveltracking and save/load

---
+++ Color Spin/Assets/Scripts/LevelStatTrack.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+[System.Serializable]
+public class LevelStatTrack : MonoBehaviour
+{
+
+    public int currLevel;
+    
+    public bool level1Complete;
+    public bool level2Complete;
+    public bool level3Complete;
+    public bool level4Complete;
+    public bool level5Complete;
+    public bool level6Complete;
+
+   
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currLevel = SceneManager.GetActiveScene().buildIndex;
+
+       
+
+        GameObject[] levelCheckers = GameObject.FindGameObjectsWithTag("Tracker");
+
+        if (levelCheckers.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+        if(currLevel == 2)
+        {
+            level1Complete = true;
+        }
+        if (currLevel == 3)
+        {
+            level2Complete = true;
+        }
+        if (currLevel == 4)
+        {
+            level3Complete = true;
+        }
+        if (currLevel == 5)
+        {
+            level4Complete = true;
+        }
+        if (currLevel == 6)
+        {
+            level5Complete = true;
+        }
+
+
+
+
+
+
+    }
+
+    public void SavePlayer()
+    {
+        SaveSystem.SaveLevelProgress(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        level1Complete = data.level1Complete;
+        level2Complete = data.level2Complete;
+        level3Complete = data.level3Complete;
+        level4Complete = data.level4Complete;
+        level5Complete = data.level5Complete;
+        level6Complete = data.level6Complete;
+
+
+    }
+
+
+
+
+}
---
+++ Color Spin/Assets/Scripts/Serialization/PlayerData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+
+    public bool level1Complete;
+    public bool level2Complete;
+    public bool level3Complete;
+    public bool level4Complete;
+    public bool level5Complete;
+    public bool level6Complete;
+
+    public PlayerData (LevelStatTrack trackScript)
+    {
+        level1Complete = trackScript.level1Complete;
+        level2Complete = trackScript.level2Complete;
+        level3Complete = trackScript.level3Complete;
+        level4Complete = trackScript.level4Complete;
+        level5Complete = trackScript.level5Complete;
+        level6Complete = trackScript.level6Complete;
+
+
+
+    }
+
+}
